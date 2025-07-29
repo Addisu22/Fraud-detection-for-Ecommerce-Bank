f5f655a7861927ce4731d89acd88fc1bdfe8b70e
Addisu22 <liveaddisu@gmail.com>
2025-07-29 21:59:49 +0300

    Data Analysis and Preprocessing : Config

---
+++ Data_Analysis/config.py
@@ -0,0 +1,5 @@
+import os
+
+data_dir = os.path.join(os.path.dirname(__file__), '../Data')
+fraud_data_path = os.path.join(data_dir, 'Fraud_Data.csv')
+ip_data_path = os.path.join(data_dir, 'IpAddress_to_Country.csv')
